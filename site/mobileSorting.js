4da53885181c83c2946a0070afab3538134deaff
chikchicly <chikchicly@gmail.com>
2022-02-19 21:17:19 +0700

    с

--- site/mobileSorting.js
+++ site/mobileSorting.js
@@ -1,6 +1,6 @@
 if(window.innerWidth<700){
   document.querySelector('#zagolovok700').innerHTML=`<strong style="padding:3%;">Пиликан</strong>`;
   document.querySelector('#zagolovok').innerHTML=``;
-  document.querySelector('#zagolovokTwo700').innerHTML=`<strong>Алада Колекшин</strong>`;
+  document.querySelector('#zagolovokTwo700').innerHTML=`<strong>Торговое оборудование</strong>`;
   document.querySelector('#zagolovokTwo').innerHTML=``;
 }
