209a20c61dfedeb95e22a3ada13c8fb3da6fa3b2
chikchicly <chikchicly@gmail.com>
2022-02-19 12:36:48 +0700

    alt and JS

---
+++ site/mobileSorting.js
@@ -0,0 +1,6 @@
+if(window.innerWidth<700){
+  document.querySelector('#zagolovok700').innerHTML=`<strong style="padding:3%;margin:10px;">Пиликан</strong>`;
+  document.querySelector('#zagolovok').innerHTML=``;
+  document.querySelector('#zagolovokTwo700').innerHTML=`<strong>Алада Колекшин</strong>`;
+  document.querySelector('#zagolovokTwo').innerHTML=``;
+}
