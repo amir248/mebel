83a2bf59130fcdf9ca61eea29771c5e5a3b16109
chikchicly <chikchicly@gmail.com>
2022-02-21 15:09:55 +0700

    doc

--- site/mobileSorting.js
+++ site/mobileSorting.js
@@ -5,8 +5,18 @@ if(window.innerWidth<700){
   document.querySelector('#zagolovokTwo').innerHTML=``;
   document.querySelector('#tele').innerHTML=``;
   document.querySelector('#tele7').innerHTML=`<a href="tel:+79528807647" style="font-size:20px;">Заказать</a>`;
+    //teleTwo
   document.querySelector('#telTwo').innerHTML=``;
+  let teleTwo=document.querySelector('#teleTwo');
   document.querySelector('#teleTwo').innerHTML=`<a href="tel:+79528807647" style="font-size:20px;">Заказать</a>`;
+  teleTwo.querySelector("a").style.cssText=`
+  color:violet;
+  margin:3%;
+  font-size:25px;
+  text-shadow:1px 1px 1px black;
+  text-decoration:none;
+  `;
+    //teleT
   let teleT=document.querySelector("#teleT");
   document.querySelector('#telT').innerHTML=``;
   document.querySelector('#teleT').innerHTML=`<a href="tel:+79528807647" style="font-size:20px;">Заказать</a>`;
@@ -14,6 +24,6 @@ if(window.innerWidth<700){
   teleT.querySelector("a").style.cssText=`
   margin:3%;
   color:green;
-  font-size:35px;
+  font-size:25px;
   `;
 }
