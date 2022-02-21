497370b415791e7d27a3e54991127fdc5bcdb591
chikchicly <chikchicly@gmail.com>
2022-02-21 09:45:53 +0700

    new Commit

--- site/mobileSorting.js
+++ site/mobileSorting.js
@@ -3,4 +3,8 @@ if(window.innerWidth<700){
   document.querySelector('#zagolovok').innerHTML=``;
   document.querySelector('#zagolovokTwo700').innerHTML=`<strong>Торговое оборудование</strong>`;
   document.querySelector('#zagolovokTwo').innerHTML=``;
+  document.querySelector('#tele').innerHTML=``;
+  document.querySelector('#tele7').innerHTML=`<a href="tel:+79528807647" style="font-size:20px;">Заказать</a>`;
+  document.querySelector('#telTwo').innerHTML=``;
+  document.querySelector('#teleTwo').innerHTML=`<a href="tel:+79528807647" style="font-size:20px;">Заказать</a>`;
 }
