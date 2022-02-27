f8961aa18a896997e78ba4463cef5fb37e2d39eb
amir248 <chikchicly@yandex.ru>
2022-02-27 08:09:14 +0700

    nachalo

---
+++ site/newCreateAside.js
@@ -0,0 +1,9 @@
+if(window.innerWidth>700){
+  console.log(">700");
+  let aside=document.createElement('aside');
+  let main=document.querySelector('main');
+  main.append(aside);
+  document.querySelector('aside').innerHTML=`
+  O_o mainGarebal;
+  `;
+}
