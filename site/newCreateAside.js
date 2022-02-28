0834c7987ef91418da788a3d5cb6953373ea5869
amir248 <chikchicly@yandex.ru>
2022-02-28 09:52:28 +0700

    Yyy

--- site/newCreateAside.js
+++ site/newCreateAside.js
@@ -54,8 +54,8 @@ if(window.innerWidth>1100){
 }
 // setTimeout(()=>{
 let frame=document.querySelector('#gChatFrame');
-let htmlO=document.querySelector('html');
-let bodyO=document.querySelector('body');
+let htmlO=document.querySelector('#gChatFrame > html');
+let bodyO=document.querySelector('#gChatFrame > body');
 htmlO.append(bodyO);
 bodyO.append(frame);
 frame.append('.chatContainer').style.cssText=`
